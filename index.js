01a0a008fe4b9bb4e2a7774e8734d9911b26fd49
Daniel Antunes <daniel.antunes1697@gmail.com>
2018-01-16 14:13:22 -0200

    ajuste

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-//const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000; //porta padrão
-//const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000; //porta padrão
+const mysql = require('mysql');
 
 //configurando o body parser para pegar POSTS mais tarde
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //definindo as rotas
 const router = express.Router();
@@ -14,7 +14,7 @@ router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 app.use('/', router);
 
 //inicia o servidor
-app.listen(PORT);
+app.listen(port);
 console.log('API funcionando!');
 
 // function execSQLQuery(sqlQry, res) {
