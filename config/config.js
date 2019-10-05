e81c2e835a472c47d7268f66d1209cbd08fa5cc2
Daniel Antunes <daniel.antunes@superlogica.com>
2019-10-05 17:28:49 -0300

    - Add production config

--- config/config.js
+++ config/config.js
@@ -7,10 +7,6 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'mysql'
+    use_env_variable: 'DATABASE_URL'
   }
 }
