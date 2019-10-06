3811b4d7ce6c458c3141c679c8340cf5748ce35f
Daniel Antunes <daniel.antunes@superlogica.com>
2019-10-06 19:01:07 -0300

    - Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
     es6: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ["standard"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaVersion: 2018
-  },
-  rules: {
   }
 }
--- src/database/seeders/20191005160222-users.js
+++ src/database/seeders/20191005160222-users.js
@@ -1,23 +1,39 @@
 'use strict'
 
 module.exports = {
-  up: queryInterface => queryInterface.bulkInsert('Users',
-    [
-      {
-        name: 'Renard Doe',
-        email: 'renard@gmail.com',
-        password: '9ff7b641722c30acdc058f2499d97dd8',
-        createdAt: new Date().toJSON().slice(0, 19).replace('T', ' '),
-        updatedAt: new Date().toJSON().slice(0, 19).replace('T', ' ')
-      },
-      {
-        name: 'John Travolta',
-        email: 'jhon@gmail.com',
-        password: '082b66a712e3efe31385f3158e057496',
-        createdAt: new Date().toJSON().slice(0, 19).replace('T', ' '),
-        updatedAt: new Date().toJSON().slice(0, 19).replace('T', ' ')
-      }
-    ], {}),
+  up: queryInterface =>
+    queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          name: 'Renard Doe',
+          email: 'renard@gmail.com',
+          password: '9ff7b641722c30acdc058f2499d97dd8',
+          createdAt: new Date()
+            .toJSON()
+            .slice(0, 19)
+            .replace('T', ' '),
+          updatedAt: new Date()
+            .toJSON()
+            .slice(0, 19)
+            .replace('T', ' ')
+        },
+        {
+          name: 'John Travolta',
+          email: 'jhon@gmail.com',
+          password: '082b66a712e3efe31385f3158e057496',
+          createdAt: new Date()
+            .toJSON()
+            .slice(0, 19)
+            .replace('T', ' '),
+          updatedAt: new Date()
+            .toJSON()
+            .slice(0, 19)
+            .replace('T', ' ')
+        }
+      ],
+      {}
+    ),
 
   down: queryInterface => queryInterface.bulkDelete('Users', null, {})
 }
